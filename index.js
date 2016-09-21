e4e85725101b85b4362c078720c259b259edfd79
Vivek Ratkalkar <vratkalkar@live.com>
2016-09-21 14:45:49 -0400

    Add all files for now

--- index.js
+++ index.js
@@ -2,8 +2,21 @@ var express = require('express');
 
 var app = express();
 
+var stormpath = require('express-stormpath');
+
+console.log("The server has started");
+
+app.use(stormpath.init(app, {
+	expand: {
+		customData: true,
+	},
+	web: {
+		produces: ['application/json']
+	}
+}))
+
 app.get('/notes', function(req, res) {
-	res.json({notes: "This is your notebook. Edit this to start saving your notes!."})
+	res.json({notes: req.user.customData.notes || "This is your notebook. Edit this to start saving your notes!."})
 })
 
 app.listen(3000);
