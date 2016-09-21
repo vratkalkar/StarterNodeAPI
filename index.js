2180ae9d86c1373bb44affb79700faf9d27e8748
Vivek Ratkalkar <vratkalkar@live.com>
2016-09-21 16:08:39 -0400

    Add body-parser to index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var express = require('express');
-
 var app = express();
-
 var stormpath = require('express-stormpath');
+var bodyParser = require('body-parser');
 
 console.log("The server has started");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(stormpath.init(app, {
 	expand: {
@@ -15,7 +17,17 @@ app.use(stormpath.init(app, {
 	}
 }))
 
-app.get('/notes', function(req, res) {
+app.post('/notes', stormpath.apiAuthenticationRequired, function(req, res) {
+	if(!req.body.notes || typeof req.body.notes != "string") {
+		res.status(400).send("400 Bad Request");
+	}
+
+	req.user.customData.notes = req.body.notes;
+	req.user.customData.save();
+	res.status(200).end();
+})
+
+app.get('/notes', stormpath.apiAuthenticationRequired, function(req, res) {
 	res.json({notes: req.user.customData.notes || "This is your notebook. Edit this to start saving your notes!."})
 })
 
